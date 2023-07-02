ea51e73e91e515de9ad35977c605d4afcd8813f1
niklas <niklas@uk-macbook.local>
2023-07-02 13:37:26 +0200

    feat: added main site todo done idea list switching

---
+++ src/routes/+page.server.js
@@ -0,0 +1,8 @@
+import { tasks } from './data';
+
+export function load() {
+    // TODO: here could be a fancy load function
+    return {
+        tasks: tasks
+    };
+}
---
+++ src/routes/data.js
@@ -0,0 +1,60 @@
+
+export const tasks = [
+    {
+        task: 'Buy milk',
+        status: 'todo',
+    },
+    {
+        task: 'Buy food',
+        status: 'todo',
+    },
+    {
+        task: 'Buy bread',
+        status: 'todo',
+    },
+    {
+        task: 'Buy drings',
+        status: 'todo',
+    },
+    {
+        task: 'Buy toys',
+        status: 'done',
+    },
+    {
+        task: 'Buy something',
+        status: 'idea',
+    }
+]
+
+// possible statuses of a task
+// export const statuses = {
+//     idea: 'idea',
+//     done: 'done',
+//     today: 'today'
+// };
+// export const tasks = [
+//     {
+//         highPriority: true,
+//         title: 'Buy milk',
+//         description: 'Go to the store and buy milk',
+//         status: statuses.idea
+//     },
+//     {
+//         highPriority: false,
+//         title: 'Buy bread',
+//         description: 'Go to the store and buy bread',
+//         status: statuses.idea
+//     },
+//     {
+//         highPriority: false,
+//         title: 'Buy eggs',
+//         description: 'Go to the store and buy eggs',
+//         status: statuses.idea
+//     },
+//     {
+//         highPriority: false,
+//         title: 'Buy butter',
+//         description: 'Go to the store and buy butter',
+//         status: statuses.idea
+//     }
+// ];
